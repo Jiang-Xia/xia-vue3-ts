010f7d42a99146947712204f498e1f0c780e31b3
JiangChuanJun <JiangChuanJun@deepaint.cn>
2020-10-09 14:19:42 +0800

    初始化

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  lintOnSave: process.env.NODE_ENV !== 'production',
+  devServer: {
+    open: true, // 启动项目后自动开启浏览器
+    overlay: { // 是否开启协议名,如果开启会发出警告
+      warnings: true,
+      errors: false
+    },
+    host: '0.0.0.0',
+    port: 3333
+  }
+}
