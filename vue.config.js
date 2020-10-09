bac75d117a2756785d213071792a8e4b92d98a7d
JiangChuanJun <JiangChuanJun@deepaint.cn>
2020-10-09 16:49:00 +0800

    eslint

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
   devServer: {
     open: true, // 启动项目后自动开启浏览器
     overlay: { // 是否开启协议名,如果开启会发出警告
-      warnings: true,
+      warnings: false,
       errors: false
     },
     host: '0.0.0.0',
